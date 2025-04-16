8523e87c1d934fa64680970a0f2824fbd1fe7106
Denian <denianszz@gmail.com>
2025-04-16 18:55:24 -0300

    first commit

---
+++ src/Ecommerce.Domain/Entities/Produto.cs
@@ -0,0 +1,10 @@
+﻿// src/Ecommerce.API/Models/Produto.cs
+namespace Ecommerce.API.Models;
+
+public class Produto
+{
+    public int Id { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public decimal Preco { get; set; }
+}
